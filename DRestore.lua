ed00ff74432b4df7445d50bf17d4ba0e16c32605
CountDracula <moldoboss27@gmail.com>
2026-05-05 09:43:32 -0700

    Create DRestore.lua

---
+++ DRestore.lua
@@ -0,0 +1,37 @@
+local Players = game:GetService("Players")
+local player = Players.LocalPlayer
+
+local function getf3x()
+    for _, v in ipairs(player.Backpack:GetChildren()) do
+        if v:FindFirstChild("SyncAPI") then
+            return v
+        end
+    end
+    for _, v in ipairs(player.Character:GetChildren()) do
+        if v:FindFirstChild("SyncAPI") then
+            return v
+        end
+    end
+end
+
+local function instantDecalRestore()
+    local f3x = getf3x()
+    if not f3x then 
+        return 
+    end
+
+    local endpoint = f3x.SyncAPI.ServerEndpoint
+    local targetId = "125385946470583"
+
+    for _, v in ipairs(workspace:GetDescendants()) do
+        if v:IsA("Decal") and string.find(v.Texture or "", targetId) then
+            task.spawn(function()
+                pcall(function()
+                    endpoint:InvokeServer("Remove", {v})
+                end)
+            end)
+        end
+    end
+end
+
+instantDecalRestore()
